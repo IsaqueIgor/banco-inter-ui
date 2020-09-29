0acde80909f997c9263f6a88888799c114b6990a
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-09-29 20:05:30 -0300

    feat: add line chart to account Summary

--- src/pages/Dashboard/MainContent/AccountSummary/index.tsx
+++ src/pages/Dashboard/MainContent/AccountSummary/index.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 
+import { ResponsiveLine } from '@nivo/line';
 import { FiCreditCard, FiFileText, FiEye, FiEyeOff } from 'react-icons/fi';
+import { useTheme } from 'styled-components';
 
+import lineChartData from '../../../../utils/line-chart-data';
 import { i18n } from '../../../../translate/i18n';
 import {
   Container,
@@ -11,14 +14,26 @@ import {
   LeftData,
   RightData,
   DataValue,
+  CustomTooltip,
 } from './styles';
 import Button from '../../../../components/Button';
 import CreditCardIllustration from '../../../../assets/images/illustrations/card-illustration.png';
 import { PlataformaPaiIcon } from '../../../../assets/images/icons';
 
+type ChartValue = number | React.ReactText | undefined;
+
+const formatChartValue = (value: ChartValue): string => `${value || 0}%`;
+
 const AccountSummary: React.FC = () => {
   const [displayStatement, setDisplayStatement] = useState(true);
   const [displayInvestments, setDisplayInvestments] = useState(true);
+  const [investmentGrowth, setInvestmentGrowth] = useState(() => {
+    const [investments] = lineChartData;
+    const { y } = investments.data[investments.data.length - 1];
+    return formatChartValue(y);
+  });
+
+  const { colors } = useTheme();
 
   return (
     <Container>
@@ -35,7 +50,48 @@ const AccountSummary: React.FC = () => {
         </Header>
 
         <DataWrapper>
-          <LeftData>{i18n.t('account.graph')}</LeftData>
+          <LeftData>
+            {' '}
+            <ResponsiveLine
+              data={lineChartData}
+              useMesh
+              enableArea
+              enableCrosshair={false}
+              curve="cardinal"
+              margin={{ top: 8, right: 8, bottom: 20, left: 8 }}
+              xScale={{ type: 'point' }}
+              yScale={{
+                type: 'linear',
+                min: 'auto',
+                max: 'auto',
+                stacked: true,
+                reverse: false,
+              }}
+              tooltip={({ point }) => {
+                return (
+                  <CustomTooltip>
+                    {formatChartValue(point.data.yFormatted)}
+                  </CustomTooltip>
+                );
+              }}
+              axisTop={null}
+              axisRight={null}
+              axisBottom={{
+                orient: 'bottom',
+                tickSize: 0,
+                tickPadding: 8,
+                tickRotation: 0,
+              }}
+              axisLeft={null}
+              colors={colors.success}
+              lineWidth={1.5}
+              pointSize={8}
+              pointColor={colors.success}
+              pointLabel="y"
+              pointLabelYOffset={-12}
+              enableGridY={false}
+            />
+          </LeftData>
           <RightData>
             <span>{i18n.t('account.revenue')}</span>
             <DataValue income>
@@ -79,12 +135,55 @@ const AccountSummary: React.FC = () => {
         </Header>
 
         <DataWrapper>
-          <LeftData>{i18n.t('account.graph')}</LeftData>
+          <LeftData>
+            {' '}
+            <ResponsiveLine
+              data={lineChartData}
+              useMesh
+              enableArea
+              enableCrosshair={false}
+              curve="cardinal"
+              margin={{ top: 8, right: 8, bottom: 20, left: 8 }}
+              xScale={{ type: 'point' }}
+              yScale={{
+                type: 'linear',
+                min: 'auto',
+                max: 'auto',
+                stacked: true,
+                reverse: false,
+              }}
+              tooltip={({ point }) => {
+                return (
+                  <CustomTooltip>
+                    {formatChartValue(point.data.yFormatted)}
+                  </CustomTooltip>
+                );
+              }}
+              axisTop={null}
+              axisRight={null}
+              axisBottom={{
+                orient: 'bottom',
+                tickSize: 0,
+                tickPadding: 8,
+                tickRotation: 0,
+              }}
+              axisLeft={null}
+              colors={colors.success}
+              lineWidth={1.5}
+              pointSize={8}
+              pointColor={colors.success}
+              pointLabel="y"
+              pointLabelYOffset={-12}
+              enableGridY={false}
+            />
+          </LeftData>
           <RightData>
             <span>{i18n.t('investments.investedCapital')}</span>
             <DataValue>{displayInvestments ? 'R$ 5.750,00' : '---'}</DataValue>
             <span>{i18n.t('investments.monthPerfomance')}</span>
-            <DataValue>{displayInvestments ? '20%' : '---'}</DataValue>
+            <DataValue>
+              {displayInvestments ? investmentGrowth : '---'}
+            </DataValue>
           </RightData>
         </DataWrapper>
       </Card>
