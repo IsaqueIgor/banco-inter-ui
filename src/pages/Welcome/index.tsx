a0b47b6098a49ca1b266a9fcc25742a7863de3b4
Isaque Igor <isaqueigorsilva@gmail.com>
2020-11-01 16:32:41 -0300

    feat: define default layouts

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 
+import { useTheme } from 'styled-components';
 import { useHistory } from 'react-router-dom';
+import { transform } from 'framer-motion';
 
 import { AnimatedContainer, Form, SignInButton } from './styles';
+import AuthLayout from '../_layouts/auth';
 import useAuth from '../../contexts/auth';
+import { ANIMATION } from './animations';
 import { BancoInter } from '../../assets/images';
 
 const Welcome: React.FC = () => {
@@ -11,6 +15,11 @@ const Welcome: React.FC = () => {
 
   const history = useHistory();
   const { signIn } = useAuth();
+  const { colors } = useTheme();
+
+  const inputRange = [0, 4];
+  const outputRange = [colors.lightGrey, colors.primary];
+  const animateBackground = transform(inputRange, outputRange);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setInputValue(e.target.value);
@@ -23,26 +32,33 @@ const Welcome: React.FC = () => {
   };
 
   return (
-    <AnimatedContainer>
-      <h1>
-        <BancoInter />
-      </h1>
-      <h2>Welcome to the redesign of the interface of Banco Inter.</h2>
-      <Form onSubmit={handleFormSubmit}>
-        <label htmlFor="name">
-          <input
-            type="text"
-            name="name"
-            placeholder="Enter your name"
-            value={inputValue}
-            onChange={handleInputChange}
-          />
-        </label>
-        <SignInButton type="submit" disabled={!inputValue}>
-          Sign In
-        </SignInButton>
-      </Form>
-    </AnimatedContainer>
+    <AuthLayout background={animateBackground(inputValue.length)}>
+      <AnimatedContainer
+        variants={ANIMATION}
+        initial="unMounted"
+        animate="mounted"
+        exit="unMounted"
+      >
+        <h1>
+          <BancoInter />
+        </h1>
+        <h2>Welcome to the redesign of the interface of Banco Inter.</h2>
+        <Form onSubmit={handleFormSubmit}>
+          <label htmlFor="name">
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter your name"
+              value={inputValue}
+              onChange={handleInputChange}
+            />
+          </label>
+          <SignInButton type="submit" disabled={!inputValue}>
+            Sign In
+          </SignInButton>
+        </Form>
+      </AnimatedContainer>
+    </AuthLayout>
   );
 };
 
---
+++ src/pages/_layouts/auth/index.tsx
@@ -0,0 +1,29 @@
+import React, { PropsWithChildren } from 'react';
+
+import { useTheme } from 'styled-components';
+
+import { AnimatedContainer } from './styles';
+
+export type AuthLayoutProps = {
+  background: string;
+};
+
+const AuthLayout = (
+  props: PropsWithChildren<AuthLayoutProps>,
+): React.ReactElement => {
+  const { background, children } = props;
+  const { colors } = useTheme();
+
+  return (
+    <AnimatedContainer
+      initial={false}
+      animate={{ background }}
+      exit={{ background: colors.lightGrey }}
+      transition={{ duration: 0.35 }}
+    >
+      {children}
+    </AnimatedContainer>
+  );
+};
+
+export default AuthLayout;
